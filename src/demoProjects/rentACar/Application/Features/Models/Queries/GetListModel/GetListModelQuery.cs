900520629b9ddc690a87639397273525f428ca50
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2022-09-19 00:36:38 +0300

    Complex Modelling & Dynamic Search

---
+++ src/demoProjects/rentACar/Application/Features/Models/Queries/GetListModel/GetListModelQuery.cs
@@ -0,0 +1,45 @@
+﻿using Application.Features.Models.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Models.Queries.GetListModel
+{
+    public class GetListModelQuery:IRequest<ModelListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListModelQueryHandler : IRequestHandler<GetListModelQuery, ModelListModel>
+        {
+            private readonly IMapper _mappper;
+            private readonly IModelRepository _modelRepository;
+
+            public GetListModelQueryHandler(IMapper mappper, IModelRepository modelRepository)
+            {
+                _mappper = mappper;
+                _modelRepository = modelRepository;
+            }
+
+            public async Task<ModelListModel> Handle(GetListModelQuery request, CancellationToken cancellationToken)
+            {
+               IPaginate<Model> models=await  _modelRepository.GetListAsync(include:
+                                               m => m.Include(c=>c.Brand),
+                                               index:request.PageRequest.Page,
+                                               size:request.PageRequest.PageSize
+                                               );
+
+                ModelListModel mappedModels = _mappper.Map<ModelListModel>(models);
+                return mappedModels;
+            }
+        }
+    }
+}
